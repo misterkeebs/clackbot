f3c07b7c3d23411f56a5c6d2dc40bc5f22548be8
Felipe Coury <felipe.coury@gmail.com>
2020-11-02 22:29:29 -0300

    feat: sleeps between spawner runs

--- spawner.js
+++ spawner.js
@@ -4,16 +4,23 @@ require('dotenv').config();
 const twitch = require('./src/TwitchClient');
 const ClackSpawner = require('./src/ClackSpawner');
 
+const sleep = process.env.SLEEP_BETWEEN_SPAWNS || 30;
+
 async function main() {
   const client = await twitch.connect();
   const spawner = new ClackSpawner(client);
   await spawner.check();
 }
 
-main().then(_ => {
-  console.log('All done.');
-  process.exit(0);
-}).catch(err => {
-  console.error('Error', err);
-  process.exit(1);
-});
+function run() {
+  console.log('Starting spawner...');
+  main().then(_ => {
+    console.log(`All done, waiting ${sleep}s before next run...\n`);
+  }).catch(err => {
+    console.error('Error', err);
+  }).finally(_ => {
+    setTimeout(run, sleep * 1000);
+  });
+}
+
+run();
