d9f2d44ff8c9bf9e92752fdd4dda656544626601
Felipe Coury <felipe.coury@gmail.com>
2020-11-05 22:37:00 -0300

    test: fixed Bot spec

--- test/Bot.spec.js
+++ test/Bot.spec.js
@@ -1,16 +1,21 @@
-const { expressJwtSecret } = require('jwks-rsa');
 const Bot = require('../src/Bot');
 const { expect } = require('chai');
 
 describe('Bot', () => {
   it('handles commands', () => {
-    let message = null;
+    let theMessage = null;
     const iface = {};
 
     const bot = new Bot();
-    bot.registerCommand('pegar', (_i, _c, _u, msg) => message = msg);
-    bot.handleMessage(iface, 'srteclados', 'fcoury', '!pegar');
+    bot.registerCommands({
+      pegar: (iface, { message }) => theMessage = message,
+    });
+    bot.handleMessage(iface, {
+      channel: 'srteclados',
+      user: 'fcoury',
+      message: '!pegar',
+    });
 
-    expect(message).to.eql('!pegar');
+    expect(theMessage).to.eql('!pegar');
   });
 });
