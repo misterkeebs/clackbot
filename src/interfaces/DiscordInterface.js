71130b008ce7dea7e67f6230f1f601cc92aeb0e2
Felipe Coury <felipe.coury@gmail.com>
2020-11-16 22:56:31 -0300

    feat: does not embed the link when confirming user

--- src/interfaces/DiscordInterface.js
+++ src/interfaces/DiscordInterface.js
@@ -37,7 +37,7 @@ class DiscordInterface {
       if (!handler) return;
 
       if (!user) {
-        return msg.reply(`você precisa linkar a sua conta do Twitch. Prá isso, visite o chat do SrTeclados, visitando https://www.twitch.tv/popout/SrTeclados/chat, e digitando \`!eusou ${userName}\``);
+        return msg.reply(`você precisa linkar a sua conta do Twitch. Prá isso, visite o chat do SrTeclados, visitando <https://www.twitch.tv/popout/SrTeclados/chat>, e digitando \`!eusou ${userName}\``);
       }
 
       try {
