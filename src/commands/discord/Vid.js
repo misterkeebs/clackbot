f5a5a09f73175c46a86cc013c6ad8480cc1e4b5b
Felipe Coury <felipe.coury@gmail.com>
2020-11-24 11:06:02 -0300

    fix: error when not found

--- src/commands/discord/Vid.js
+++ src/commands/discord/Vid.js
@@ -6,19 +6,19 @@ async function findVid(t) {
   let r = await yts.search(`MrKeebs ${terms}`);
   const mrKeebsVid = r.videos.find(v => v.author && v.author.name === 'MrKeebs');
 
-  if (mrKeebsVid.title.toLowerCase().includes(terms)) {
+  if (mrKeebsVid && mrKeebsVid.title.toLowerCase().includes(terms)) {
     return mrKeebsVid;
   }
 
   r = await yts.search(`SrTeclados ${terms}`);
   const srTecladosVid = r.videos.find(v => v.author && v.author.name === 'SrTeclados');
 
-  if (srTecladosVid.title.toLowerCase().includes(terms)) {
+  if (srTecladosVid && srTecladosVid.title.toLowerCase().includes(terms)) {
     return srTecladosVid;
   }
 }
 
-const Vids = async (iface, { channel, user: userName, message, rawMessage={} }) => {
+const Vids = async (iface, { channel, user: userName, message }) => {
   const terms = message.split(' ').slice(1).join(' ');
   const video = await findVid(terms);
 
