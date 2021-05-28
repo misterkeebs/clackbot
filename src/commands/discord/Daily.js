8625eb91f9d0b0a2db9d10f40b6b12c94bb6e321
Felipe Coury <felipe.coury@gmail.com>
2021-05-27 21:46:54 -0300

    fix: next daily is usually displayed with wrong language

--- src/commands/discord/Daily.js
+++ src/commands/discord/Daily.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+moment.locale('pt-br');
 
 const AlreadyRedeemedError = require('../../models/AlreadyRedeemedError');
 const User = require('../../models/User');
@@ -15,7 +16,6 @@ const Daily = async (iface, { channel, user: userName, message, rawMessage = {} 
     return await iface.reply(channel, userName, `${msg.join(' ')}!`);
   } catch (err) {
     if (err instanceof AlreadyRedeemedError) {
-      moment.locale('pt-br');
       const nextSlot = moment(err.nextSlot).fromNow();
       return await iface.reply(channel, userName, `você já pegou seu daily hoje. Você pode pegar de novo ${nextSlot}.`);
     }
