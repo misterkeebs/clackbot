306c0501595e1dfbe10211127f0ae18c2fdac2da
Felipe Coury <felipe.coury@gmail.com>
2021-07-16 13:30:37 -0300

    chore: removed .only

--- test/commands/Comandos.spec.js
+++ test/commands/Comandos.spec.js
@@ -11,7 +11,7 @@ class Test {
   static interfaces = ['discord', 'twitch'];
 }
 
-describe.only('Comandos', () => {
+describe('Comandos', () => {
   beforeEach(async () => {
     iface.bot = {
       handlers: {
