db3063fd3ab2ff6ec5c2ba07a411bb54ceaa3e65
Felipe Coury <felipe.coury@gmail.com>
2021-08-15 01:27:14 +0000

    feat: added test db creation

--- scripts/post_create.sh
+++ scripts/post_create.sh
@@ -4,3 +4,11 @@ yarn install
 curl https://cli-assets.heroku.com/install.sh | sh
 git remote add heroku https://git.heroku.com/clackbot.git
 cp .env.codespace .env
+sudo apt-get update
+sudo apt-get install -y postgresql-client
+echo "CREATE DATABASE clackbot_test;" | psql
+echo "CREATE ROLE clackbot_test LOGIN PASSWORD 'clackbot';" | psql
+echo "GRANT CONNECT ON DATABASE clackbot_test TO clackbot_test;" | psql
+echo "GRANT USAGE ON SCHEMA public TO clackbot_test;" | psql
+echo "GRANT ALL PRIVILEGES ON DATABASE clackbot_test TO clackbot_test;" | psql
+yarn knex migrate:latest
