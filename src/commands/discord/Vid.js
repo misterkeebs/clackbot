6bd627c639e998815884ed8b86f2990d253f57c6
Felipe Coury <felipe.coury@gmail.com>
2024-04-14 12:09:13 -0300

    fix: new deployment

--- src/commands/discord/Vid.js
+++ src/commands/discord/Vid.js
@@ -1,17 +1,21 @@
-const yts = require('yt-search');
-const _ = require('lodash');
+const yts = require("../../lib/vid-search");
+const _ = require("lodash");
 
 async function findVid(t) {
   const terms = t.toLowerCase();
   let r = await yts.search(`MrKeebs ${terms}`);
-  const mrKeebsVid = r.videos.find(v => v.author && v.author.name === 'MrKeebs');
+  const mrKeebsVid = r.videos.find(
+    (v) => v.author && v.author.name === "MrKeebs",
+  );
 
   if (mrKeebsVid && mrKeebsVid.title.toLowerCase().includes(terms)) {
     return mrKeebsVid;
   }
 
   r = await yts.search(`SrTeclados ${terms}`);
-  const srTecladosVid = r.videos.find(v => v.author && v.author.name === 'SrTeclados');
+  const srTecladosVid = r.videos.find(
+    (v) => v.author && v.author.name === "SrTeclados",
+  );
 
   if (srTecladosVid && srTecladosVid.title.toLowerCase().includes(terms)) {
     return srTecladosVid;
@@ -19,15 +23,19 @@ async function findVid(t) {
 }
 
 const Vids = async (iface, { channel, user: userName, message }) => {
-  const terms = message.split(' ').slice(1).join(' ');
+  const terms = message.split(" ").slice(1).join(" ");
   const video = await findVid(terms);
 
   if (!video) {
-    return await iface.reply(channel, userName, `nenhum vídeo com ${terms} encontrado`);
+    return await iface.reply(
+      channel,
+      userName,
+      `nenhum vídeo com ${terms} encontrado`,
+    );
   }
 
   return await iface.reply(channel, userName, `${video.title} - ${video.url}`);
 };
-Vids.interfaces = ['discord'];
+Vids.interfaces = ["discord"];
 
 module.exports = Vids;
--- src/interfaces/DiscordInterface.js
+++ src/interfaces/DiscordInterface.js
@@ -1,11 +1,11 @@
-const _ = require('lodash');
-const Promise = require('bluebird');
+const _ = require("lodash");
+const Promise = require("bluebird");
 
-const User = require('../models/User');
-const BuySell = require('../processors/BuySell');
-const SlowModeProcessor = require('../processors/SlowMode');
-const ImgOnly = require('../processors/ImageOnly');
-const Voting = require('../processors/Voting');
+const User = require("../models/User");
+const BuySell = require("../processors/BuySell");
+const SlowModeProcessor = require("../processors/SlowMode");
+const ImgOnly = require("../processors/ImageOnly");
+const Voting = require("../processors/Voting");
 
 const PROCESSORS = [
   new Voting(),
@@ -18,11 +18,11 @@ class DiscordInterface {
   constructor(bot, client) {
     this.bot = bot;
     this.client = client;
-    this.name = 'discord';
+    this.name = "discord";
 
-    this.client.on('messageReactionAdd', async (reaction, user) => {
+    this.client.on("messageReactionAdd", async (reaction, user) => {
       let resolved = false;
-      await Promise.map(PROCESSORS, async proc => {
+      await Promise.map(PROCESSORS, async (proc) => {
         if (!_.isFunction(proc.handleReaction)) return;
         if (resolved) return;
         if (reaction.partial) {
@@ -34,52 +34,63 @@ class DiscordInterface {
       });
     });
 
-    this.client.on('message', async msg => {
+    this.client.on("message", async (msg) => {
       const processed = await this.preProcess(client, msg);
       if (processed) return;
       if (msg.author.bot) return;
-      if (!msg.content.startsWith('!')) return;
+      if (!msg.content.startsWith("!")) return;
 
-      console.log('msg', msg.content);
+      console.log("msg", msg.content);
 
       const { author } = msg;
-      const [rawCommand, _params] = msg.content.split(' ');
+      const [rawCommand, _params] = msg.content.split(" ");
       const command = rawCommand.slice(1);
       const userName = `${author.username}#${author.discriminator}`;
 
       const discordId = msg.author.id;
-      console.log(' ***', discordId);
+      console.log(" ***", discordId);
       const [user] = await User.query()
-        .where('discordId', discordId)
-        .orWhere('discordWannabe', userName);
+        .where("discordId", discordId)
+        .orWhere("discordWannabe", userName);
 
-      console.log('command', command);
+      console.log("command", command);
       const handler = bot.getCommand(command);
       if (!handler) return;
 
       if (!user) {
-        return msg.reply(`você precisa linkar a sua conta do Twitch. Prá isso, visite o chat do SrTeclados, visitando <https://www.twitch.tv/popout/SrTeclados/chat>, e digitando \`!eusou ${userName}\``);
+        return msg.reply(
+          `você precisa linkar a sua conta do Twitch. Prá isso, visite o chat do SrTeclados, visitando <https://www.twitch.tv/popout/SrTeclados/chat>, e digitando \`!eusou ${userName}\``,
+        );
       }
 
       try {
-        await this.bot.handleMessage(this, { channel: msg.channel.id, user: user.displayName, message: msg.content, rawMessage: msg });
+        await this.bot.handleMessage(this, {
+          channel: msg.channel.id,
+          user: user.displayName,
+          message: msg.content,
+          rawMessage: msg,
+        });
       } catch (err) {
         // eslint-disable-next-line no-console
-        console.error('Error on', command, err);
-        this.reply(msg.channel.id, user.displayName, `houve um erro de processamento no seu comando: ${err}`);
+        console.error("Error on", command, err);
+        this.reply(
+          msg.channel.id,
+          user.displayName,
+          `houve um erro de processamento no seu comando: ${err}`,
+        );
       }
     });
   }
 
   async reply(channelId, dbUser, message, attachment) {
-    const channel = this.client.channels.cache.find(c => c.id === channelId);
+    const channel = this.client.channels.cache.find((c) => c.id === channelId);
     const displayName = dbUser.displayName || dbUser;
     if (Number.isFinite(dbUser)) {
       return channel.send(`${dbUser} ${message}`, attachment);
     }
 
     if (_.isString(displayName)) {
-      const [user] = await User.query().where('displayName', displayName);
+      const [user] = await User.query().where("displayName", displayName);
       if (user.discordId) {
         return channel.send(`<@${user.discordId}> ${message}`, attachment);
       }
@@ -89,19 +100,19 @@ class DiscordInterface {
   }
 
   async send(channelId, message, attachment) {
-    const channel = this.client.channels.cache.find(c => c.id === channelId);
+    const channel = this.client.channels.cache.find((c) => c.id === channelId);
     return channel.send(`${message}`, attachment);
   }
 
   async preProcess(client, msg) {
     let resolved = false;
-    const result = await Promise.map(PROCESSORS, async proc => {
+    const result = await Promise.map(PROCESSORS, async (proc) => {
       if (resolved) return Promise.resolve();
       const res = await proc.handle(msg);
       if (res) resolved = true;
       return res;
     });
-    return result.find(r => !!r);
+    return result.find((r) => !!r);
   }
 }
 
